a2a83c6b09769b63f76622cd76b8e2e931b37387
kevinsorey <48227187+kevinsorey@users.noreply.github.com>
2020-05-05 23:15:29 -0400

    StoreFront

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import HomePage from './homepage.component';
+import './homepage.style.scss';
+
+function App() {
+  return (
+    <div>
+        
+        <HomePage/>
+    </div>
+  );
+}
+
+export default App;
